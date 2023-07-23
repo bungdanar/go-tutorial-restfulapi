4453d30ea2542bbff91876bea6049fb156b18dd1
Bung Danar <danar.g.putera@gmail.com>
2023-07-23 20:19:55 +0700

    Database Migration

--- app/database.go
+++ app/database.go
@@ -7,7 +7,7 @@ import (
 )
 
 func NewDB() *sql.DB {
-	db, err := sql.Open("mysql", "root:brightshield!23@tcp(localhost:3306)/go-tutorial-restfulapi")
+	db, err := sql.Open("mysql", "root:brightshield!23@tcp(localhost:3306)/go-tutorial-migration")
 	helper.PanicIfError(err)
 
 	db.SetMaxIdleConns(5)
